7522028df9d6cfddb489c266a7c785388d22ab18
Aisha Jackson <aisha.jackson@nike.com>
2019-06-01 12:16:41 -0700

    conditional rendering

--- src/app/Conditional.js
+++ src/app/Conditional.js
@@ -1,14 +1,7 @@
 import React from "react"
 
 function Conditional(props) {
-    // condition ? statement if true : statement if false
-
-    return (
-        <div>
-            <h1>Some cool stuff about conditional rendering</h1>
-        </div>
-    )
-    
+    return <h1>Some cool stuff about conditional rendering</h1>
 }
 
 export default Conditional
